622007584eabc7583890b17b71f1b3b5fc73b695
Natalia Fijałkowska <natalkakoziel@gmail.com>
2022-12-21 16:41:56 +0100

    [#5] create part of models and migrations seeding data

--- t9a/models.py
+++ t9a/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Deployments(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
+
+
+class Secondaries(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
+
+
+class Army(models.Model):
+    name = models.CharField(max_length=16)
+    long_name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
+
+
+class Map(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
+
+
+
+
