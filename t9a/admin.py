940f59ab8026a203c62e7bfedf711e1380cd601c
Natalia Fijałkowska <natalkakoziel@gmail.com>
2022-12-22 14:29:59 +0100

    [#9] create models lists, games and results; add all models to django admin

--- t9a/admin.py
+++ t9a/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Games)
+admin.site.register(models.Lists)
+admin.site.register(models.Results)
+admin.site.register(models.Army)
+admin.site.register(models.Deployments)
+admin.site.register(models.Map)
+admin.site.register(models.Secondaries)
+
--- t9a/models.py
+++ t9a/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Deployments(models.Model):
@@ -30,5 +31,35 @@ class Map(models.Model):
         return f'{self.id}. {self.name}'
 
 
+class Lists(models.Model):
+    owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    army = models.ForeignKey('Army', null=True, on_delete=models.SET_NULL)
+    list = models.TextField()
+    name = models.CharField(max_length=256)
 
 
+class Games(models.Model):
+    date = models.DateField()
+    map = models.ForeignKey('Map', null=True, on_delete=models.SET_NULL)
+    deploy = models.ForeignKey('Deployments', null=True, on_delete=models.SET_NULL)
+    secondary = models.ForeignKey('Secondaries', null=True, on_delete=models.SET_NULL)
+    turns = models.IntegerField()
+    event = models.CharField(max_length=256)
+    points_event = models.IntegerField()
+
+
+class Results(models.Model):
+    choice_list = [
+        (1, 'winner'),
+        (2, 'draw'),
+        (3, 'loose')
+    ]
+    game = models.ForeignKey('Games', null=True, on_delete=models.SET_NULL)
+    player = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    secondary = models.IntegerField(choices=choice_list)
+    score = models.IntegerField()
+    result = models.IntegerField(choices=choice_list)
+    list = models.ForeignKey('Lists', null=True, on_delete=models.SET_NULL)
+    comment = models.TextField(blank=True)
+    points = models.IntegerField()
+    approved = models.BooleanField(null=True)
